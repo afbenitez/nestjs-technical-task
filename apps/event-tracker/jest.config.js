ecc9cb1bf97cd7267d913f4fd483b21743734c3c
afbenitez <af.benitez@uniandes.edu.co>
2025-10-20 14:46:06 +0200

    fix route in jest test

--- apps/event-tracker/jest.config.js
+++ apps/event-tracker/jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
+  rootDir: '.',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
+  coverageDirectory: './coverage',
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^@domain/(.*)$': '<rootDir>/domain/$1',
-    '^@application/(.*)$': '<rootDir>/application/$1',
-    '^@infrastructure/(.*)$': '<rootDir>/infrastructure/$1',
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@application/(.*)$': '<rootDir>/src/application/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
   },
 };
